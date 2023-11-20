8eeacd0126f96983c88c8e47337461e5f0eac8a2
Your Name <nguyenducanhtuan0602@gmail.com>
2023-11-20 14:26:50 +0700

    Add project files.

---
+++ StookBore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StookBore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
