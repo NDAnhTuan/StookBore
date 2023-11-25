92434e076505394d07a6500cd55411e4df211665
RenIVII <96332154+RenIVII@users.noreply.github.com>
2023-11-25 21:51:50 +0700

    Add files via upload
    change data types

--- StookBore/databaseSQL/create_table.sql
+++ StookBore/databaseSQL/create_table.sql
@@ -1,8 +1,9 @@
 use sql12664900;
+
 CREATE TABLE Users (
     user_id VARCHAR(10),
-    email VARCHAR(10),
-    password_key VARCHAR(10),
+    email VARCHAR(30),
+    password_key VARCHAR(12),
     first_name VARCHAR(10),
     last_name VARCHAR(10),
     phone_number VARCHAR(10),
@@ -11,76 +12,76 @@ CREATE TABLE Users (
 
 CREATE TABLE Users_Address (
     user_id VARCHAR(10),
-    street VARCHAR(10),
-    district VARCHAR(10),
-    city VARCHAR(10),
-    country VARCHAR(10),
+    street VARCHAR(20),
+    district VARCHAR(20),
+    city VARCHAR(20),
+    country VARCHAR(15)
 );
 
 CREATE TABLE Employees (
-    user_id VARCHAR(10),
+    user_id VARCHAR(10)
 );
 
 CREATE TABLE Clients (
     user_id VARCHAR(10),
-    level VARCHAR(10),
-    vip_point VARCHAR(10)
+    levels INT,
+    vip_point INT
 );
 
 CREATE TABLE Stock_Contains (
     book_id VARCHAR(10),
     stock_order_id VARCHAR(10),
-    quantify VARCHAR(10),
+    quantity INT
 );
 
 CREATE TABLE Stock_Order (
     stock_order_id VARCHAR(10),
-    producer VARCHAR(10),
-    payment_method VARCHAR(10),
-    paid_date VARCHAR(10),
+    producer VARCHAR(30),
+    payment_method VARCHAR(20),
+    paid_date DATE,
     employee_id VARCHAR(10)
 );
 
 CREATE TABLE Books (
     book_id VARCHAR(10),
-    title VARCHAR(10),
-    description VARCHAR(10),
-    publisher VARCHAR(10),
-    stock VARCHAR(10),
-    current_price VARCHAR(10),
-    edition_version VARCHAR(10),
+    title VARCHAR(50),
+    descriptions VARCHAR(200),
+    publisher VARCHAR(30),
+    stock INT,
+    current_price INT,
+    edition_version VARCHAR(20),
     category_id VARCHAR(10)
 );
 
 CREATE TABLE Contain (
     order_id VARCHAR(10),
     book_id VARCHAR(10),
-    quantify VARCHAR(10)
+    quantity INT
 );
 
 CREATE TABLE Category (
     category_id VARCHAR(10),
-    category_name VARCHAR(10)
+    category_name VARCHAR(20)
 );
 
 CREATE TABLE Author_Dept (
     book_id VARCHAR(10),
-    author VARCHAR(10)
+    author VARCHAR(50)
 );
 
 CREATE TABLE Orders (
     order_id VARCHAR(10),
     order_status VARCHAR(10),
-    shipment_method VARCHAR(10),
-    shipment_date VARCHAR(10),
-    shipment_price VARCHAR(10),
+    shipment_method VARCHAR(20),
+    shipment_date DATE,
+    shipment_price INT,
     shipment_status VARCHAR(10),
-    street VARCHAR(10),
-    district VARCHAR(10),
-    city VARCHAR(10),
-    country VARCHAR(10),
-    payment_method VARCHAR(10),
-    paid_date VARCHAR(10),
+    street VARCHAR(20),
+    district VARCHAR(20),
+    city VARCHAR(20),
+    country VARCHAR(15),
+    payment_method VARCHAR(20),
+    paid_date DATE,
     client_id VARCHAR(10)
 );
 
@@ -92,32 +93,32 @@ CREATE TABLE Manage (
 CREATE TABLE Exchange (
     user_id VARCHAR(10),
     promotion_id VARCHAR(10),
-    date VARCHAR,
-    quantify VARCHAR    
+    dates date,
+    quantity INT
 );
 
 CREATE TABLE Promotion (
     promotion_id VARCHAR(10),
-    name VARCHAR(10),
-    start_valid_date VARCHAR,
-    expired_date VARCHAR,
-    quantity VARCHAR,
-    promotion_type VARCHAR   
+    names VARCHAR(10),
+    start_valid_date DATE,
+    expired_date DATE,
+    quantity INT,
+    promotion_type VARCHAR(10)
 );
 
 CREATE TABLE Gift (
     promotion_id VARCHAR(10),
-    number_of_use VARCHAR(10),
-    sale_off_amount VARCHAR(10),
-    times_used VARCHAR(10)
+    number_of_use INT,
+    sale_off_amount INT,
+    times_used INT
 );
 
 CREATE TABLE Discount (
     promotion_id VARCHAR(10),
-    vip_point_cost VARCHAR(10),
-    sale_off_amount VARCHAR(10),
-    max_money_sale_off VARCHAR(10),
-    amount VARCHAR
+    vip_point_cost INT,
+    sale_off_amount INT,
+    max_money_sale_off INT,
+    amount INT
 );
 
 CREATE TABLE Apply (
