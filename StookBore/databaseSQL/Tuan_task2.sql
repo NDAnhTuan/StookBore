3837985b5d5bcdbfb49f80a3cd52d97d03082e19
NDAnhTuan <89686737+NDAnhTuan@users.noreply.github.com>
2023-11-29 23:02:22 +0700

    add ship_price + sub_total  Tuan_task2.sql

--- StookBore/databaseSQL/Tuan_task2.sql
+++ StookBore/databaseSQL/Tuan_task2.sql
@@ -6,7 +6,8 @@ DROP TRIGGER IF EXISTS check_promotion_validity;
 
 
 CREATE TEMPORARY TABLE grand_total_view (
-	order_id INT NOT NULL, 
+	order_id INT NOT NULL,
+    sub_total INT NOT NULL, 
 	grand_total INT NOT NULL
 );
 
@@ -19,15 +20,20 @@ CREATE TRIGGER order_update_grand_total
 AFTER INSERT ON Orders
 FOR EACH ROW
 BEGIN
-declare temp INT default 0;
+DECLARE temp INT DEFAULT 0;
+DECLARE ship_temp DOUBLE DEFAULT 0;
 SET temp = (
 	SELECT SUM(current_price * quantity)
     FROM Contain
     INNER JOIN Books ON Contain.book_id = Books.book_id
     WHERE Contain.order_id = NEW.order_id);
-    
-	INSERT INTO grand_total_view (order_id, grand_total)
-    VALUES ( NEW.order_id,temp);
+
+SET ship_temp = (
+	SELECT
+		shipment_price
+	FROM Orders WHERE order_id = NEW.order_id);
+	INSERT INTO grand_total_view (order_id, sub_total, grand_total)
+    VALUES ( NEW.order_id, temp, temp + ship_temp);
 END //
 delimiter ;
 
@@ -46,6 +52,12 @@ BEGIN
     IF valid_date THEN
 		BEGIN
 			DECLARE check_type BOOLEAN;
+            DECLARE ship_temp DOUBLE DEFAULT 0;
+            
+            SET ship_temp = (SELECT
+				shipment_price
+			 FROM Orders WHERE order_id = NEW.order_id);
+             
             SET check_type = (SELECT
 				promotion_type 
 			 FROM Promotion WHERE promotion_id = NEW.promotion_id);
@@ -53,13 +65,14 @@ BEGIN
 				BEGIN
 					DECLARE amount_grant DOUBLE DEFAULT 0;
                     DECLARE grant_temp DOUBLE DEFAULT 0;
+                    
                     SET amount_grant = (
                     SELECT
 						sale_off_amount
 						FROM Gift WHERE promotion_id = NEW.promotion_id AND number_of_use > times_used);
 					SET grant_temp = (
                     SELECT
-						grand_total*(1 - amount_grant)
+						sub_total*(1 - amount_grant) + ship_temp
 					FROM grand_total_view WHERE order_id = NEW.order_id);
 
                     UPDATE  grand_total_view
@@ -81,11 +94,11 @@ BEGIN
 						FROM Discount WHERE promotion_id = NEW.promotion_id);
 					SET grant_temp = (
                     SELECT
-						IF(grand_total*amount_grant > max_amount_grant,grand_total - max_amount_grant,grand_total*(1-amount_grant))
+						IF(sub_total*amount_grant > max_amount_grant, sub_total - max_amount_grant + ship_temp, sub_total*(1-amount_grant) + ship_temp)
 					FROM grand_total_view WHERE order_id = NEW.order_id);
-
+					
                     UPDATE  grand_total_view
-                    SET grand_total = grant_temp
+                    SET grand_total = (grant_temp)
                     WHERE order_id = NEW.order_id;
                 END;
 			END IF;
