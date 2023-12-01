cc8e135d30b0c30610d4b5568884aab6781489e9
hungvo2003vn <votanhung2003@gmail.com>
2023-12-02 00:38:31 +0700

    Add edge cases

--- src/model/order/updateOrder.js
+++ src/model/order/updateOrder.js
@@ -6,7 +6,30 @@ async function updateOrder({
     shipment_status
 }) {
 
+    //Not valid type
+    if(order_id !== undefined && isNaN(parseInt(order_id))) {
+        return Promise.reject({
+            status: 404,
+            message: `Cannot find Order with order_id: ${order_id}`
+        })
+    }
+
+    if(shipment_status!== undefined && isNaN(parseInt(shipment_status))) {
+        return Promise.reject({
+            status: 503,
+            message: `Cannot update Order with shipment_status: ${shipment_status}`
+        })
+    }
+
     const [result, fields] = await connection.query(`SELECT * FROM Orders WHERE order_id=${order_id}`)
+
+    //Not exist
+    if(result.length <=0){
+        return Promise.reject({
+            status: 404,
+            message: `Cannot find Order with order_id: ${order_id}`
+        })
+    }
 
     const {
         shipment_method,
